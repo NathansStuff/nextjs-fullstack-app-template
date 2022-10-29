55db69913b59079229ac491494f81276cb8cce03
NathansStuff <n.odonnell@hotmail.com>
2022-10-29 20:20:01 +1100

    feat: add initial viewports to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,14 @@
-import 'tailwindcss/tailwind.css';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { Provider } from 'react-redux';
+import 'tailwindcss/tailwind.css';
 import { store } from '../src/context/store';
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+const customViewports = {
+  vroom: {
+    name: 'VROOM 1.8 Ratio',
+    styles: {
+      width: '400px',
+      height: '720px',
     },
   },
 };
@@ -19,3 +20,33 @@ export const decorators = [
     </Provider>
   ),
 ];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      ...customViewports,
+    },
+    defaultViewport: 'vroom',
+  },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: '#ffffff',
+      },
+      {
+        name: 'dark',
+        value: '#000000',
+      },
+    ],
+  },
+};
